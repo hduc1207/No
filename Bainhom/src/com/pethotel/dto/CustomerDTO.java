f38a4aca479639780e6549e54174a4c0ec555ff4
hduc1207 <hduc3357@gmail.com>
2026-01-01 15:52:46 +0700

    Add files via upload

--- Bainhom/src/com/pethotel/dto/CustomerDTO.java
+++ Bainhom/src/com/pethotel/dto/CustomerDTO.java
@@ -10,6 +10,54 @@ public class CustomerDTO {
 
     public CustomerDTO() {}
 
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
     public CustomerDTO(int customerId, String fullName, String phoneNumber, String email, String address, String note) {
         this.customerId = customerId;
         this.fullName = fullName;
@@ -18,9 +66,4 @@ public class CustomerDTO {
         this.address = address;
         this.note = note;
     }
-
-    public int getCustomerId() { return customerId; }
-    public void setCustomerId(int customerId) { this.customerId = customerId; }
-    public String getFullName() { return fullName; }
-    public void setFullName(String fullName) { this.fullName = fullName; }
 }
